05f94c046cdd2f6061eee86ccf88dd39d5b06513
Krzysztof Buszewicz <krzysztof.buszewicz@gmail.com>
2025-12-31 18:57:42 +0100

    initial commit

---
+++ spec/marisa_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Marisa do
+  it "has a version number" do
+    expect(Marisa::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
